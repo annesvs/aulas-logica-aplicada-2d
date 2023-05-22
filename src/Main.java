1bdad0f3206fe60f4ab93967f07f81257f7ada38
annesvs <anna7silvas@gmail.com>
2023-05-22 12:30:40 -0300

    apaguei tudo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         // Aula incial de Orientação á objeto
-                       Pessoa adao = new Pessoa();
+        Pessoa adao = new Pessoa();
+        // Declaração
+        Pessoa qualquer;
+        // Instanciação
+        qualquer = new Pessoa();
+        // Definir forma do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        // Definir comportamento
+        qualquer.falar();
+        System.out.println(qualquer.falar("alto"));
     }
     }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,3 +1,21 @@
 public class Pessoa {
     // Atributos
+    String nome;
+    String sobrenome;
+
+    // Métodos
+    public void falar() {
+        System.out.println("falei");
+    }
+    // Sobrecarga do método falar()
+    public String falar(String volume){
+        return "falei" + volume;
+    }
+    // Construtor
+    Pessoa(){
+
+    }
+    Pessoa(String _nome){
+        this.nome = _nome;
+    }
 }
